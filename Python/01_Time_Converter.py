67d2c2c8208326df25ec621dce416c9567faef71
Jammu Usha Sree Asmitha <jammuusha7@gmail.com>
2026-04-09 11:00:03 +0530

    Update 01_Time_Converter.py

--- Python/01_Time_Converter.py
+++ Python/01_Time_Converter.py
@@ -1,12 +1,9 @@
-def convert_minutes(total_minutes):
-    hours = total_minutes // 60
-    minutes = total_minutes % 60
-    if hours == 0:
-        return f"{minutes} minutes"
-    elif minutes == 0:
-        return f"{hours} hrs"
-    else:
-        return f"{hours} hrs {minutes} minutes"
-test_values = [130, 110, 60, 45, 200, 0]
-for val in test_values:
-    print(f"{val} minutes = {convert_minutes(val)}")
+total_minutes = int(input("Enter minutes: "))
+hours = total_minutes // 60
+minutes = total_minutes % 60
+if hours == 0:
+    print(str(minutes) + " minutes")
+elif minutes == 0:
+    print(str(hours) + " hrs")
+else:
+    print(str(hours) + " hrs " + str(minutes) + " minutes")
